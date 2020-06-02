482d1c0083b84c2cb74d403c904edde7df4f2148
Sai Jeevan Balla <41351501+saijeevanballa@users.noreply.github.com>
2020-06-02 17:02:49 +0530

    modified

--- numbers/module.js
+++ numbers/module.js
@@ -8,19 +8,34 @@ module.exports.getNumbers = getNumbers;
 
 const likeIncrement = async function(id) {
   let data = await numDB.findById(id);
-  return Object.assign(data, { likes: data.likes++ }).save();
+  return await numDB.findByIdAndUpdate(
+    id,
+    { likes: data.likes + 1 },
+    { new: true }
+  );
+  // Object.assign(data, { likes: data.likes++ }).save();
 };
 module.exports.likeIncrement = likeIncrement;
 
 const viewIncrement = async function(id) {
   let data = await numDB.findById(id);
-  return Object.assign(data, { views: data.views++ }).save();
+  return await numDB.findByIdAndUpdate(
+    id,
+    { views: data.views + 1 },
+    { new: true }
+  );
+  //Object.assign(data, { views: data.views++ }).save();
 };
 module.exports.viewIncrement = viewIncrement;
 
 const disLikeIncrement = async function(id) {
   let data = await numDB.findById(id);
-  return Object.assign(data, { disLikes: data.disLikes++ }).save();
+  return await numDB.findByIdAndUpdate(
+    id,
+    { disLikes: data.disLikes + 1 },
+    { new: true }
+  );
+  //Object.assign(data, { disLikes: data.disLikes++ }).save();
 };
 module.exports.disLikeIncrement = disLikeIncrement;
 
